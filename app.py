2d584e4492eb72057a5921a82e2ac70977953e48
root <root@bastion.ab27.example.opentlc.com>
2018-10-12 00:16:36 +0000

    initial version

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+	return "Welcome Home!"
+	
+	
+@app.route('/how are you')
+def hello():
+	return "I am doing good. How about you ?"
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", port=8080)
